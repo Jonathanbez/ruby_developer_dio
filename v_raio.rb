cc06af960a6a353708221cb79718c735146ce1bd
Jonathanbez <jbbarroz@gmail.com>
2024-03-17 21:31:34 -0300

    Course Ruby Developer DIO

--- v_raio.rb
+++ v_raio.rb
@@ -2,5 +2,5 @@ PI = 3.14159
 raio = gets().to_f
 
 #TODO:  Crie as condições necessárias para o programa calcular o volume
-VOLUME = (4.0/3)*PI*raio
-puts "VOLUME = #{VOLUME}"
+volume = (4.0/3)*PI*raio**3
+puts "VOLUME = #{volume.round(3)}"
