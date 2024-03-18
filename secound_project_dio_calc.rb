5f188ef4184db7a4bc6a03c0f248217f5e3c38a0
Jonathanbez <jbbarroz@gmail.com>
2024-03-18 07:39:35 -0300

    project DIO

---
+++ secound_project_dio_calc.rb
@@ -0,0 +1,45 @@
+#PEQUENA CALCULADORA COM RUBY
+
+system "clear"
+
+puts "O que deseja calcular?\nEscolha uma das opções:\n1.Adição, 2.Subtração, 3.Multiplicação, 4.Divisão ou 0.Sair\n"
+
+loop do
+  case = option = gets.chomp.to_i
+    when option == 1
+        print "Começando com a Adição\n"
+        print "Digite o promeiro número inteiro (adição): "
+        add1 = gets.chomp.to_i
+        print "Digite o segundo número inteiro (adição): "
+        add2 = gets.chomp.to_i
+        addition = add1 + add2
+        puts "O resultado da adição é #{addition}"
+    when option == 2
+        print "Agora vamos Subtrair!\n"
+        print "Digite o minuendo inteiro(subtração): "
+        sub1 = gets.chomp.to_i
+        print "Digite o subtraendo inteiro(subtração):"
+        sub2 = gets.chomp.to_i
+        subtraction = sub1 - sub2
+        puts "O resultado da subtração é #{subtraction}"
+    when option == 3
+        print "Agora vamos Multiplicar\n"
+        print "Digite o primeiro fator, número inteiro (multiplicação): "
+        mult1 = gets.chomp.to_i
+        print "Digite o segundo fator, número inteiro (multiplicação): "
+        mult2 = gets.chomp.to_i
+        multiplication = mult1 * mult2
+        puts "O resultado da multiplicação, o produto é #{multiplication}"
+    when option == 4
+        print "Agora vamos dividir\n"
+        print "Digite o dividendo (divisão):"
+        div1 = gets.chomp.to_f
+        print "Digite o divisor (divisão):"
+        div2 = gets.chomp.to_f
+        division = div1 / div2
+        puts "O resultado da divisão é #{division}"
+    when option == 0
+      break
+    end
+  end
+end
