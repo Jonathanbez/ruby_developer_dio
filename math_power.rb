4c05d5e51e42a6587e2ada295e6523c9554ca448
Jonathanbez <jbbarroz@gmail.com>
2024-03-22 08:14:26 -0300

    Course Ruby Developer DIO/desafio potencia

--- math_power.rb
+++ math_power.rb
@@ -11,7 +11,7 @@ array.push(gets.chomp.to_i)
 puts "Digite o terceiro numeor:"
 array.push(gets.chomp.to_i)
 puts array
-power = array.map do |p|
-  p ** 3
+power = array.each do |p|
+  puts p ** 3
 end
 puts "#{power}"
