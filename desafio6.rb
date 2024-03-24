eea0826ac288ea79842c57542090410df372eb7f
Jonathanbez <jbbarroz@gmail.com>
2024-03-24 17:25:49 -0300

    Course Ruby Developer DIO/desafio basico #2

--- desafio6.rb
+++ desafio6.rb
@@ -3,11 +3,13 @@ number1 = gets.to_i
 number2 = gets.to_i
 
 def diff(number1, number2)
-  if number1 < number2
+  if number1 > number2
     print "O número #{number1} é o maior e o #{number2} é o menor"
   elsif
-    number1 > number2
-    print "O número #{number1} é o maior e o #{number2} é o menor"
+    number1 < number2
+    print "O número #{number2} é o maior e o #{number1} é o menor"
+  else
+    print "As entradas são iguais!"
   end
 end
 diff(number1, number2)
