e241aa385df02c3b13712c775e4df8fe9da50409
Jonathanbez <jbbarroz@gmail.com>
2024-03-24 16:29:27 -0300

    Course Ruby Developer DIO/desafio consulta CPF

---
+++ cpf_consult.rb
@@ -0,0 +1,14 @@
+require 'cpf_cnpj'
+
+puts "VERIFICADOR DE CPF\n\n"
+def validate_cpf(cpf)
+    if CPF.valid?(cpf, strict: true)
+        puts "CPF Valido"
+    else
+        puts "CPF invalido."
+    end
+end
+
+puts "Insira um CPF valido: "
+cpf = gets.chomp
+puts validate_cpf(cpf)
