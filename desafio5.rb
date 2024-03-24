061f8c505c285d90b0d110df3786d4a35744a91b
Jonathanbez <jbbarroz@gmail.com>
2024-03-24 17:18:15 -0300

    Course Ruby Developer DIO/desafio consulta CPF

--- desafio5.rb
+++ desafio5.rb
@@ -6,7 +6,7 @@ num = 25
 def clima(number, num)
   if number > num
     puts "Está calor!"
-  elsif number >= 21 || number <= num
+  elsif number >= 21 && number <= num
     puts "A temperatura esta agradavel!"
   else
     puts "Esta frio!"
---
+++ desafio6.rb
@@ -0,0 +1,13 @@
+
+number1 = gets.to_i
+number2 = gets.to_i
+
+def diff(number1, number2)
+  if number1 < number2
+    print "O número #{number1} é menor que #{number2}"
+  elsif
+    number1 > number2
+    print "O número #{number1} é maior que #{number2}"
+  end
+end
+diff(number1, number2)
