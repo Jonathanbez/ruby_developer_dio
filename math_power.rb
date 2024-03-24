57947e9ad1eb08335f1a115d21a78eb8ce7569e7
Jonathanbez <jbbarroz@gmail.com>
2024-03-23 22:23:37 -0300

    Course Ruby Developer DIO/desafio potencia

--- math_power.rb
+++ math_power.rb
@@ -4,13 +4,15 @@ system 'clear'
 array = []
 
 puts "Insira 3 numero (digite um numero e aperte enter)"
-puts "Digite o primeiro numero:"
+puts "\nDigite o primeiro numero:"
 array.push(gets.chomp.to_i)
-puts "Digite o segundo numero:"
+puts "\nDigite o segundo numero:"
 array.push(gets.chomp.to_i)
-puts "Digite o terceiro numeor:"
+puts "\nDigite o terceiro numeor:"
 array.push(gets.chomp.to_i)
 
-power = array.each do |p|
-  puts p ** 3
+power = array.map! do |p|
+  p ** 3
 end
+
+puts "\nResultado:\n1. Potenciação: #{array[0]}\n2. Potenciação: #{array[1]}\n3.Potenciação: #{array[2]}"
