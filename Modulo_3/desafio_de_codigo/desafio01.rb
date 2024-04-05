62a5aa46abebb3fa3e9996ea0eb07087e8f3d1b8
Jonathanbez <jbbarroz@gmail.com>
2024-04-05 08:18:47 -0300

    Course Ruby Developer DIO / Desafio 3

---
+++ Modulo_3/desafio_de_codigo/desafio01.rb
@@ -0,0 +1,13 @@
+array01 = [1,2,3,4,5]
+array02 = [1,2,3,4,5]
+
+array03 = [2,3,10,11,16,17]
+array04 = [1,2,3,4,5,6]
+
+array001 = []
+array001.push(array01[0] + array02[0], array01[2] + array02[2], array01[4] + array02[4])
+p array001
+
+array002 = []
+array002.push(array03[0] + array04[0], array03[2] + array04[2], array03[4] + array04[4])
+p array002
