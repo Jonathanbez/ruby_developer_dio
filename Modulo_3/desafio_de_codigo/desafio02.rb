4eea22a5f14d706fcc5268140fed860c677f8e26
Jonathanbez <jbbarroz@gmail.com>
2024-04-07 20:00:43 -0300

    Course Ruby Developer DIO / Desafio 3.1

--- Modulo_3/desafio_de_codigo/desafio02.rb
+++ Modulo_3/desafio_de_codigo/desafio02.rb
@@ -1,15 +1,12 @@
-precoDoGas = gets.to_i
 imposto = 0.1
+precoDoGas = gets.to_i
 impostoCobrado = gets.to_i
 impostoVariavel = gets.to_i
-# total = 0
-if impostoCobrado == 1
-  impostoVariavelDecimal = impostoVariavel * 0.1
-  sub_total = precoDoGas * impostoVariavelDecimal * imposto
-  total = sub_total + precoDoGas
-else impostoCobrado == 0
-  sub_total = precoDoGas * imposto
-  total = sub_total + precoDoGas
+
+valorTotal = precoDoGas + (imposto * (precoDoGas / 100.0) * 100)
+if impostoCobrado == 1 then
+    valorTotal += (valorTotal / 100.0) * impostoVariavel
 end
 
-puts "O preço do gás nesse mês é de R$ #{total.round(2)}"
+valorTotal = valorTotal.to_s.chop.to_i if valorTotal.to_s[-1] == "0"
+puts "O preço do gás nesse mês é de R$#{valorTotal}"
